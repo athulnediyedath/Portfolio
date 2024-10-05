9d7e38eef21e8f0e0fbbf2010ca63826989e6148
Athul Sasikumar <athulnediyedath@gmail.com>
2024-10-05 12:48:18 +0530

    updated app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,22 @@ import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
+import { defaultValues, ThemeContext, ThemeContextType } from "../styles/theme/theme";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState(defaultValues.theme);
+  const [mode, setMode] = useState(defaultValues.mode);
+  const initialValues: ThemeContextType = {
+      mode,
+      theme,
+      setTheme,
+      setMode
+  }
+
+  return (<ThemeContext.Provider value={initialValues}>
+      <Component {...pageProps} />
+  </ThemeContext.Provider>);
 }
 
 export default MyApp;
