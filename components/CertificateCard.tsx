2de5d1ce1f65a446fff34e4313dbb0f764b5901e
Athul Sasikumar <athulnediyedath@gmail.com>
2024-10-05 12:27:55 +0530

    added CertificateCard.tsx

---
+++ components/CertificateCard.tsx
@@ -0,0 +1,57 @@
+
+import React from 'react';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  Col,
+} from 'reactstrap';
+import Fade from 'react-reveal/Fade';
+import { CertificateType } from '../types/sections';
+
+const CertificateCard = ({
+  name,
+  img,
+  des,
+  link,
+}: CertificateType) => {
+  return (
+    <Col lg="6">
+      <Fade left duration={2000}>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <Card
+          style={{ flex: 1 }}
+          className="shadow-lg--hover mb-3 shadow border-0 text-center rounded"
+        >
+          <CardBody className="">
+            <img
+              src={img}
+              style={{
+                objectFit: 'cover',
+                left: 0,
+                right: 0,
+                top: '7rem',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                width: '28rem',
+                height: '18rem',
+                borderRadius: '5%',
+              }}
+            />
+            <CardTitle tag="h4" className="mb-2 mt-4">
+              {name}
+            </CardTitle>
+            
+            <CardText tag="div" className="description my-3 text-center">
+              {des}
+            </CardText>
+          </CardBody>
+        </Card>
+        </a>
+      </Fade>
+    </Col>
+  );
+};
+
+export default CertificateCard;
